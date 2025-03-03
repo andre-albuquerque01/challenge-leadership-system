c64086a2cb5fe39030a252808f4456ed06c2e590
Andre <andrealbuquerque2001@gmail.com>
2025-03-03 12:36:48 -0300

    feat: create

---
+++ Api/database/migrations/2024_11_12_204736_create_regions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->ulid('idRegion')->primary();
+            $table->string('cep');
+            $table->string('address');
+            $table->string('house');
+            $table->string('neighborhood');
+            $table->foreignUlid('userId')->references('idUser')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('regions');
+    }
+};
