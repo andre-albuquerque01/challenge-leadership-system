9892d2f9317f7026da34121ea127609a1386170a
Andre <andrealbuquerque2001@gmail.com>
2025-03-04 13:05:23 -0300

    feat: create

---
+++ Api/database/migrations/2024_11_12_214255_create_assignments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->ulid('idAssignment')->primary();
+            // $table->ulid('idMember');
+            // $table->ulid('idLeader');
+            $table->foreignUlid('idMember')->nullable()->references('idUser')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignUlid('idLeader')->nullable()->references('idUser')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignments');
+    }
+};
