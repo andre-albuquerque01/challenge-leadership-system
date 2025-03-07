3260c8178083c83bf31533f0407a842701158f38
andre <andrealbuquerque2001@gmail.com>
2025-03-07 10:23:16 -0300

    feat: create

---
+++ Api/database/factories/AssignmentsFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Assignments>
+ */
+class AssignmentsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'idMember' => Str::ulid()->toString(),
+            'idLeader' => Str::ulid()->toString(),
+        ];
+    }
+}
