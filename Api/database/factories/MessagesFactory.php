565cab0860f59d1a83bfc8d108d7ca17a91eb89f
andre <andrealbuquerque2001@gmail.com>
2025-03-07 10:23:18 -0300

    feat: create

---
+++ Api/database/factories/MessagesFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Messages>
+ */
+class MessagesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'sender_id' =>  Str::ulid()->toString(),
+            'receiver_id' =>  Str::ulid()->toString(),
+            'content' => fake()->text(),
+        ];
+    }
+}
