433d57985e40b6dae99532c6712b08d16bd5e088
andre <andrealbuquerque2001@gmail.com>
2025-03-10 09:28:13 -0300

    feat: update

--- Api/app/Models/Messages.php
+++ Api/app/Models/Messages.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Messages extends Model
 {
-    use HasUlids;
+    use HasFactory, HasUlids;
 
     protected $table = 'messages';
     protected $primaryKey = "idMessage";
@@ -15,6 +16,14 @@ class Messages extends Model
         'sender_id',
         'receiver_id',
         'content',
-        'sent_at',
     ];
+
+    public function senderMessage()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+    public function receiverMessage()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
