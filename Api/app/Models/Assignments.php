648e6c5462ff4f447fc8d4de3f808eb80f9bceec
andre <andrealbuquerque2001@gmail.com>
2025-03-10 09:28:19 -0300

    feat: update

--- Api/app/Models/Assignments.php
+++ Api/app/Models/Assignments.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Assignments extends Model
 {
-    use HasUlids;
+    use HasFactory, HasUlids;
 
     protected $table = 'assignments';
     protected $primaryKey = "idAssignment";
